9bba79a33087301afe20c23d20ca381e2af65c0a
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-10-16 10:04:54 -0600

    Cleaning up console outputs while running code

--- whisperX/whisper.py
+++ whisperX/whisper.py
@@ -30,7 +30,7 @@ model = whisperx.load_model("large-v2", device, compute_type=compute_type)
 
 audio = whisperx.load_audio(audio_file)
 result = model.transcribe(audio, batch_size=batch_size)
-print(result["segments"]) # before alignment
+# print(result["segments"]) # before alignment
 
 # delete model if low on GPU resources
 import gc
@@ -46,7 +46,7 @@ print("\nAligning with whisperX...\n")
 model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
 result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
 
-print(result["segments"]) # after alignment
+# print(result["segments"]) # after alignment
 
 # delete model if low on GPU resources
 import gc
@@ -69,8 +69,8 @@ diarize_model = whisperx.diarize.DiarizationPipeline(use_auth_token=HUGGING_FACE
 diarize_segments = diarize_model(audio)
 
 result = whisperx.assign_word_speakers(diarize_segments, result)
-print(diarize_segments)
-print(result["segments"]) # segments are now assigned speaker IDs
+# print(diarize_segments)
+# print(result["segments"]) # segments are now assigned speaker IDs
 
 diarize_end_time = datetime.now()
 print("\nFinished diarization, exporting results...\n")
