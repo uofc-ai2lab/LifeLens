fad12443a02f7715b0f763cf98aa3c22ee233d65
IshaHaider <66149883+IshaHaider@users.noreply.github.com>
2026-01-05 18:44:21 -0800

    SCRUM-133 update readme and add a script for nlp model download

--- src/main.py
+++ src/main.py
@@ -1,35 +1,34 @@
-"""
-    Main file to run microservices found in their respective folders using the command line.
-"""
-
 import argparse
+from src.services.transcription_service.transcription_whispertrt import run_transcription
+from src.services.medication_extraction_service.medication_extraction import run_medication_extraction_service
+# from src.services.intervention_extraction_service.intervention_extraction import run_intervention_extraction
+from src.services.semantic_filtering_service.semantic_filtering import run_semantic_filtering
 
 async def main():
+    """
+    Main function to run microservices found in their respective folders using the command line.
+    """
     parser = argparse.ArgumentParser(description="Run microservices")
     parser.add_argument(
         "service",
         nargs="?",
         type=str,
-        choices=["transcribe", "meds", "sem"],
+        choices=["transcribe", "meds", "inter", "sem"],
         default=None
     )
     args = parser.parse_args()
 
     if args.service == "transcribe":
-        from src.services.transcription_service.transcription_whispertrt import run_transcription
         await run_transcription()
     elif args.service == "meds":
-        from src.services.medication_extraction_service.medication_extraction import medication_extraction_service
-        await medication_extraction_service()
+        await run_medication_extraction_service()
+    # elif args.service == "inter":
+    #     await run_intervention_extraction()
     elif args.service == "sem":
-        from src.services.semantic_filtering_service.semantic_filtering import run_semantic_filtering
         await run_semantic_filtering()
     else:
-        from src.services.transcription_service.transcription_whispertrt import run_transcription
         await run_transcription()
-        from src.services.medication_extraction_service.medication_extraction import medication_extraction_service
-        await medication_extraction_service()
-        from src.services.semantic_filtering_service.semantic_filtering import run_semantic_filtering
+        await run_medication_extraction_service()
         await run_semantic_filtering()
     
 if __name__ == "__main__":
