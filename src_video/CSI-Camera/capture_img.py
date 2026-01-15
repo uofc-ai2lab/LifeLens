d3ae3f5977f1d93f6469000a8ee5d3446ab94d2b
Saba <sabayara0@gmail.com>
2026-01-15 16:53:22 -0700

    change dir

--- src_video/CSI-Camera/capture_img.py
+++ src_video/CSI-Camera/capture_img.py
@@ -2,13 +2,17 @@
 # Copyright (c) 2019-2022 JetsonHacks
 
 import cv2
-
+import os
 """ 
 gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
 Flip the image by setting the flip_method (most common values: 0 and 2)
 display_width and display_height determine the size of each camera pane in the window on the screen
 Default 1920x1080 displayd in a 1/4 size window
 """
+
+script_dir = os.path.dirname(os.path.abspath(__file__))  
+save_dir = os.path.join(script_dir, "saved_imgs")
+os.makedirs(save_dir, exist_ok=True)
 
 def gstreamer_pipeline(
     sensor_id=0,
@@ -63,7 +67,7 @@ def show_camera():
                 #Save a snapshot of the video stream when on the 'e'
                 elif keyCode == ord('e'):
                     timestamp = cv2.getTickCount()
-                    filename = f"src_video/CSI-Camera/saved_imgs/captured_img_{timestamp}.jpg"
+                    filename = os.path.join(save_dir, f"captured_img_{timestamp}.jpg")
                     cv2.imwrite(filename, frame)
                     print(f"Image saved as {filename}")
         finally:
