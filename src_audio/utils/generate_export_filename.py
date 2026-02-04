4bc99efb5bb09cf4fe7fe558381a25c2e5a3998a
Kameel Kasumu <85535423+kameelkas@users.noreply.github.com>
2026-02-04 09:20:12 -0700

    Update File. No Need to Worry About Metadata Being Updated Correctly. Isha's Upcoming PR Deals With Refactorfing Metadata Entirely

--- src_audio/utils/generate_export_filename.py
+++ src_audio/utils/generate_export_filename.py
@@ -10,7 +10,7 @@ def generate_export_filename(
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     
     # Default output filename (fallback)
-    output_filename = f"{service}_{input_file_path.parent.stem}.csv"
+    output_filename = f"{timestamp}_{service}_{input_file_path.parent.stem}.csv"
     output_file_path = input_file_path.parent / output_filename
 
     # Decide what string to search our metadata on based on service type
@@ -20,21 +20,11 @@ def generate_export_filename(
         metadata_key = "denoised_audio_path" # For transcription service, we we need to look for a denoised audio file
     else:
         metadata_key = "transcript_path" # For other services, we need to look for a transcript file
-    
-    existing_metadata = search_metadata(metadata_key, input_file_path)
-        
-    # NOT SURE WHAT IS HAPPENING HERE: ASK ISHA
-    # # If metadata exists and has an associated chunk audio, rename output accordingly
-    # if existing_metadata.:
-    #     print(f"EXIST - {existing_metadata}")
-    #     if existing_metadata.chunk_audio_path: 
-    #         output_filename = f"{timestamp}_{service}_{existing_metadata.chunk_audio_path.parent.stem}.csv"
 
     # Update metadata
     create_update_metadata(input_file_path, service, output_file_path)
 
-    # Re-fetch metadata to ensure update succeeded
-    
+    # Verify metadata update
     if search_metadata(metadata_key, input_file_path) is None:
         raise RuntimeError("Metadata update failed")
 
