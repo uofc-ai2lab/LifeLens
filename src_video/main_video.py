e3506ae44af2b7399d3fdd9c0dd93ac9a1f35fee
Saba <sabayara0@gmail.com>
2026-02-12 10:31:48 -0700

    boxMot tracking per frame

--- src_video/main_video.py
+++ src_video/main_video.py
@@ -8,6 +8,9 @@ import argparse
 from pathlib import Path
 from typing import Dict, Any, Optional
 from queue import Queue, Empty
+from ultralytics import YOLO
+from boxmot import OCSORT
+
 
 from config.jetson_startup import run_jetson_startup_tasks
 from config.resource_usage import start_monitoring, stop_monitoring
@@ -207,6 +210,14 @@ def main(video_pipeline: Optional[GStreamerVideoPipeline] = None) -> int:
         args=(image_queue, settings),
         daemon=True,
     )
+    tracker = OCSORT(
+        conf_thres=0.3,
+        iou_thres=0.3,
+        max_age=30
+    )
+
+    patient_id = None
+    person_model = YOLO("yolov8n.pt")  
 
     worker.start()
     window = "CSI Camera"
@@ -224,6 +235,21 @@ def main(video_pipeline: Optional[GStreamerVideoPipeline] = None) -> int:
     try:
         while True:
             ok, frame = video_pipeline.read_frame()
+            results = person_model.predict(frame, classes=[0], verbose=False)
+
+            detections = []
+
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+                    conf = box.conf[0].cpu().numpy()
+                    detections.append([x1, y1, x2, y2, conf, 0])  
+            tracks = tracker.update(detections, frame)
+
+
+            detections = np.array(detections)
+
             if not ok or frame is None:
                 log.error("Camera read failed")
                 break
@@ -263,7 +289,7 @@ def main(video_pipeline: Optional[GStreamerVideoPipeline] = None) -> int:
                     log.success("Job queued")
 
 
-            draw_overlay(frame, fps, processing)
+            draw_overlay(frame, fps, processing, tracks)
             cv2.imshow(window, frame)
             
             # Single waitKey with proper ESC and 'q' handling
